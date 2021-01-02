717e8eeb8e2303b40c37b84d2dcc1cafe9057286
Sam Sheets <sam.sheets94@gmail.com>
2021-01-02 10:28:05 -0700

    implemented GET endpoints

--- src/app.js
+++ src/app.js
@@ -27,8 +27,56 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+const bookmarks = [
+    {
+      id: 0,
+      title: 'Google',
+      url: 'http://www.google.com',
+      rating: '3',
+      desc: 'Internet-related services and products.'
+    },
+    {
+      id: 1,
+      title: 'Thinkful',
+      url: 'http://www.thinkful.com',
+      rating: '5',
+      desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
+    },
+    {
+      id: 2,
+      title: 'Github',
+      url: 'http://www.github.com',
+      rating: '4',
+      desc: 'brings together the world\'s largest community of developers.'
+    }
+];
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
+})
+
+app.get('/bookmarks', (req, res) => {
+    res.json(bookmarks)
+})
+
+app.get('/bookmarks/:id', (req, res) => {
+    const {id} = req.params
+    const bookmark = bookmarks.find(b => b.id == id)
+    if(!bookmark) {
+        logger.error(`Bookmark with id ${id} not found.`)
+        return res.status(404).send('Bookmark Not Found')
+    }
+    res.json(bookmark)
 })
 
 app.use(function errorHandler(error, req, res, next) {

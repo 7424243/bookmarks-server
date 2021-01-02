05c25687e9653be1c2feb0b5f8ffa946698605c3
Sam Sheets <sam.sheets94@gmail.com>
2021-01-02 11:19:11 -0700

    refactored code to implement routes

--- src/app.js
+++ src/app.js
@@ -5,53 +5,19 @@ const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
 const winston = require('winston')
-const {v4: uuid} = require('uuid')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-const logger = winston.createLogger({
-    lvel: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({filename: 'info.log'})
-    ]
-})
-if (NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
-}
+
 
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common'
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.use(express.json())
 
-const bookmarks = [
-    {
-      id: 0,
-      title: 'Google',
-      url: 'http://www.google.com',
-      rating: '3',
-      desc: 'Internet-related services and products.'
-    },
-    {
-      id: 1,
-      title: 'Thinkful',
-      url: 'http://www.thinkful.com',
-      rating: '5',
-      desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-    },
-    {
-      id: 2,
-      title: 'Github',
-      url: 'http://www.github.com',
-      rating: '4',
-      desc: 'brings together the world\'s largest community of developers.'
-    }
-];
+
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
@@ -63,54 +29,11 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
+app.use(bookmarksRouter)
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.get('/bookmarks', (req, res) => {
-    res.json(bookmarks)
-})
-
-app.get('/bookmarks/:id', (req, res) => {
-    const {id} = req.params
-    const bookmark = bookmarks.find(b => b.id == id)
-    if(!bookmark) {
-        logger.error(`Bookmark with id ${id} not found.`)
-        return res.status(404).send('Bookmark Not Found')
-    }
-    res.json(bookmark)
-})
-
-app.post('/bookmarks', (req, res) => {
-    const {title, url, rating, desc} = req.body
-    if(!title) {
-        logger.error(`Title is required`)
-        return res.status(400).send('Invalid data')
-    }
-    if(!url) {
-        logger.error(`Url is required`)
-        return res.status(400).send('Invalid data')
-    }
-    if(!rating) {
-        logger.error(`Rating is required`)
-        return res.status(400).send('Invalid data')
-    }
-    if(!desc) {
-        logger.error(`Description is required`)
-        return res.status(400).send('Invalid data')
-    }
-    const id = uuid()
-    const bookmark = {
-        id,
-        title,
-        url,
-        rating,
-        desc
-    }
-    bookmarks.push(bookmark)
-    logger.info(`Bookmark with id ${id} created`)
-    res.status(201).location(`http://localhost:8000/bookmarks/${id}`).json(bookmark)
-})
 
 app.use(function errorHandler(error, req, res, next) {
     let response
